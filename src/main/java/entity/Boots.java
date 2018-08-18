a571c26eb58e14d032ea940af22a330e481396dc
Lukasz <lmienicki@gmail.com>
2018-08-18 23:47:35 +0200

    Switch project to maven project

---
+++ src/main/java/entity/Boots.java
@@ -0,0 +1,28 @@
+package entity;
+
+public class Boots extends Product {
+    private int size;
+    private boolean isNaturalSkin;
+
+    public Boots(int id, String productName, double price, int weight, String color, int productCount, int size, boolean isNaturalSkin) {
+        super(id, productName, price, weight, color, productCount);
+        this.size = size;
+        this.isNaturalSkin = isNaturalSkin;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean isNaturalSkin() {
+        return isNaturalSkin;
+    }
+
+    @Override
+    public String toString() {
+        return "Boots{" +
+                "size=" + size +
+                ", isNaturalSkin=" + isNaturalSkin +
+                '}';
+    }
+}
